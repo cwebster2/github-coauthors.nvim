3ce3be5edd1c2e96a2a595901ef364356f084d4b
Casey Webster <casey.webster@gmail.com>
2021-06-02 22:30:00 -0500

    simplify

--- lua/telescope/_extensions/github-coauthors.lua
+++ lua/telescope/_extensions/github-coauthors.lua
@@ -1,6 +1,32 @@
-local coauthors = require'something'
+local actions = require("telescope.actions")
+local finders = require("telescope.finders")
+local pickers = require("telescope.pickers")
+local utils = require("telescope.utils")
+local conf = require("telescope.config").values
+local sorters = require("telescope.sorters")
+local make_entry = require("telescope.make_entry")
+
 return require("telescope").register_extension {
-  exports = {
-    coauthors = coauthors,
-  },
+  export = {
+    scripts = function(opts)
+      local results = utils.get_os_command_output({
+        "git", "shortlog", "--summary", "--numbered", "--email", "--all"
+      }, opts.cwd)
+
+      pickers.new(opts, {
+        prompt_title = 'Git Coauthors',
+        finder = finders.new_table {
+          results = results,
+          entry_maker = opts.entry_maker or make_entry.gen_from_git_commits(opts),
+        },
+        previewer = previewers.git_commit_diff.new(opts),
+        sorter = conf.file_sorter(opts),
+        -- attach_mappings = function()
+        --   actions.select_default:replace(actions.git_checkout)
+        --   return true
+        -- end
+      }):find()
+    end
+  }
 }
+
